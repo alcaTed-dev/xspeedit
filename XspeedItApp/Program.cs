5c21b8a83aaae3103aa8a78792ea09981c608819
Teddy Alcabelard <alcated@outlook.fr>
2019-06-26 00:22:32 +0200

    Create Solution and Project

---
+++ XspeedItApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace XspeedItApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
